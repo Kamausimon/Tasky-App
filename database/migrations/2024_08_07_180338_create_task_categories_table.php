d375a983f390d182709de0bb95508887bd8994cb
kamau simon <kamausimon217@gmail.com>
2024-08-07 21:07:21 +0300

    created a task catagories table to help with categorizing the users tasks

--- database/migrations/2024_08_07_180338_create_task_categories_table.php
+++ database/migrations/2024_08_07_180338_create_task_categories_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('task_categories', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->string('color')->default('#000000');
+            $table->foreignId('user_id')->constrained('users');
         });
     }
 
