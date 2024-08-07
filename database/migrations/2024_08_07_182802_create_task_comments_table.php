8901d71029b08f713ada0b14bdc3d20157158997
kamau simon <kamausimon217@gmail.com>
2024-08-07 22:13:17 +0300

    made changes to the task_comments foreign constraints column

--- database/migrations/2024_08_07_182802_create_task_comments_table.php
+++ database/migrations/2024_08_07_182802_create_task_comments_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->text('comment');
+            $table->unsignedBigInteger('task_id');
+            $table->unsignedBigInteger('user_id');
+
             $table->foreign('task_id')->references('id')->on('tasks')->cascadeOnDelete();
             $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
         });
