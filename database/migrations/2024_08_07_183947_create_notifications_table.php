35e3d4ce5d277040b9c7db182421b5b577a58891
kamau simon <kamausimon217@gmail.com>
2024-08-07 22:15:17 +0300

    Add user_id column to notifications table

--- database/migrations/2024_08_07_183947_create_notifications_table.php
+++ database/migrations/2024_08_07_183947_create_notifications_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('type');
             $table->json('data');
             $table->timestamp('read_at')->nullable();
+            $table->unsignedBigInteger('user_id');
 
             $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
         });
