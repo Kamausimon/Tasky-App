40c0e42fde28bbedef8906afefa98b311ccaad76
kamau simon <kamausimon217@gmail.com>
2024-08-07 22:22:53 +0300

    chore: Add user_role pivot table

--- database/migrations/2024_08_07_185730_create_user_role_table.php
+++ database/migrations/2024_08_07_185730_create_user_role_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('user_role', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('role_id');
+
             $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
             $table->foreign('role_id')->references('id')->on('roles')->cascadeOnDelete();
         });
