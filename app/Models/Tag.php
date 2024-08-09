067b5bc6624733727383fa6fd40ccd17a8580b2c
kamau simon <kamausimon217@gmail.com>
2024-08-09 13:18:43 +0300

    refactor: Update Tag model to use 'task_tags' table and add fillable fields

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     use HasFactory;
+
+    protected $table = 'task_tags';
+
+    protected $fillable = [
+        'name',
+        'color'
+    ];
 }
